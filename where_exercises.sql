60bac44665b3f2506b619aad1f59e16d08186074
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2015-11-23 14:11:40 -0600

    learned how to chain where clauses in mysql.

--- where_exercises.sql
+++ where_exercises.sql
@@ -16,3 +16,23 @@ where birth_date LIKE ('%-12-25');
 SELECT *from employees
 where last_name LIKE ('%q%');
 
+SELECT *from employees
+where (first_name = 'Irena' or first_name= 'Vidya' or first_name= 'Maya')
+and gender = 'M';
+
+SELECT *from employees
+where (last_name like 'e%'
+or last_name like '%e');
+
+SELECT *from employees
+where (last_name like 'e%'
+and last_name like '%e');
+
+SELECT *from employees
+where (hire_date between '1990-01-01' and '1999-12-31'
+and birth_date like '%-12-25');
+
+SELECT *from employees
+where (last_name like '%q%'
+and not last_name like '%qu%');
+
